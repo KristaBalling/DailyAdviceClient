724db120325beee09bdd1815dcf507c719cc4a10
Krista Blachian <KristaBlachian@outlook.com>
2018-04-24 18:19:21 -0400

    exercise complete

---
+++ src/com/theironyard/DailyAdviceClient.java
@@ -0,0 +1,32 @@
+package com.theironyard;
+
+import java.io.*;
+import java.net.*;
+
+public class DailyAdviceClient {
+
+    public void go() {
+        try {
+            Socket s = new Socket("127.0.0.1", 4242);
+
+            InputStreamReader streamReader = new InputStreamReader(s.getInputStream());
+            BufferedReader reader = new BufferedReader(streamReader);
+            //chain a BufferedReader to an inputStreamReader to the input
+            // stream from the Socket.
+
+            String advice = reader.readLine();
+            System.out.println("Today you should: " + advice);
+
+            reader.close();
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        DailyAdviceClient client = new DailyAdviceClient();
+        client.go();
+    }
+
+
+}
